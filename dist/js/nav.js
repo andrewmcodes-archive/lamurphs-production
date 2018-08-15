b8f61c17dca2a5ac27d7001a542c30556f91ff4f
andrewmcodes <andrew.mason@n2pub.com>
2018-08-15 19:42:54 -0400

    updates

--- dist/js/nav.js
+++ dist/js/nav.js
@@ -13,31 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("js-nav-menu-button").addEventListener("click", toggleMenu)
 });
 
-function originalColor() {
-  var myNav = document.getElementById("nav");
-  window.onscroll = function () {
-    if (screen.width < 902) {
-      myNav.style.backgroundColor = "#1F9D55";
-    } else {
-      myNav.style.backgroundColor = "";
-    }
-  }
-}
-
 function toggleMenu() {
   var myNav = document.getElementById("nav");
   document.getElementById("js-nav-menu").classList.toggle("hidden");
-}
-
-function colorChange() {
-  var myNav = document.getElementById("nav");
-  window.onscroll = function () {
-    if (window.scrollY >= 200) {
-      myNav.style.backgroundColor = "#1F9D55";
-      myNav.style.boxShadow = "0 15px 30px 0 rgba(0,0,0,0.11),0 5px 15px 0 rgba(0,0,0,0.08)";
-    } else {
-      myNav.style.backgroundColor = "";
-      myNav.style.boxShadow = "";
-    }
-  };
 }
